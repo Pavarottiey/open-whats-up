58035f925aa6a6523058392bf0eeb86b122a40b5
cmsandiga <cmsandiga@gmail.com>
2012-11-11 02:51:21 -0500

    Adding contactlist layout blank

---
+++ open-whatsup/src/com/example/openwu/ContactList.java
@@ -0,0 +1,16 @@
+package com.example.openwu;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ContactList extends Activity {
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+	    super.onCreate(savedInstanceState);
+	
+	    // TODO Auto-generated method stub
+	}
+
+}
